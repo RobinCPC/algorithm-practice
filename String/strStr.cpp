5f2cc65d9dae2f671dda7949a945e061de2a87e3
robincpc <chienpinchen@gmail.com>
2016-06-19 21:53:58 -0700

    update solutions of strStr

--- String/strStr.cpp
+++ String/strStr.cpp
@@ -11,7 +11,30 @@ using namespace std;
 class Solution 
 {
     public:
-        int strStr(string haystack, string needle) 
+        int strStr(string haystack, string needle)
+        {
+            if (haystack.empty() && needle.empty())
+                return 0;
+            if (haystack.empty())
+                return -1;
+            if (haystack.length() < needle.length())
+                return -1;
+
+            for (size_t i = 0; i < haystack.size() - needle.size() + 1; i++)
+            {
+                size_t j = 0;
+                for (; j < needle.size(); j++)
+                {
+                    if (haystack[i+j] != needle[j])
+                        break;
+                }
+                if ( j == needle.size() )
+                    return i;
+            }
+            return -1;
+        }
+
+        int strStr_buildIn(string haystack, string needle) 
         {
             size_t ind = haystack.find(needle);
             if (ind != string::npos)
--- String/strStr.py
+++ String/strStr.py
@@ -12,10 +12,23 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        return haystack.find(needle)
+        if haystack is None or needle is None:
+            return -1
+        if len(haystack) < len(needle):
+            return -1
+        
+        for i in xrange(len(haystack) - len(needle) + 1 ):
+            for j in xrange(len(needle)):
+                if haystack[i+j] != needle[j]:
+                    break
+            else: # special else method : means 'no break'
+            # if j == len(needle)-1:
+                return i
+        return -1
+        #return haystack.find(needle)
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print sol.strStr('helloworld','oo')
+    print sol.strStr('helloworld','owo')
 
